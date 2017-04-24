bf3fa379fd55dce396248573d2551d9c30b2835f
xuyanpiny <xuyanpiny@163.com>
2017-04-24 22:37:14 +0800

    修改格式

--- HelloWorld/src/com/xuyp/git/mode/factory/IFactory.java
+++ HelloWorld/src/com/xuyp/git/mode/factory/IFactory.java
@@ -1,12 +1,12 @@
-package com.xuyp.git.mode.factory;
-
-import com.xuyp.git.mode.factory.impl.BMW;
-
 /*
  * File Name: IFactory.java
  * Created by xuyp on 2017年4月24日 下午5:50:01.
  * Copyright:Copyright © 1985-2017 xuyp Inc.All right reserved.
  */
+package com.xuyp.git.mode.factory;
+
+import com.xuyp.git.mode.factory.impl.BMW;
+
 public interface IFactory {
 	
 	BMW createBMW();
