2ed767d4f7cc79ca0d3fca27be062c8d234f5ee7
xuyp95 <xuyanpiny@163.com>
2017-04-25 09:29:28 +0800

    合并远程代码

--- HelloWorld/src/com/xuyp/git/mode/factory/impl/Factory510.java
+++ HelloWorld/src/com/xuyp/git/mode/factory/impl/Factory510.java
@@ -1,16 +1,20 @@
-package com.xuyp.git.mode.factory.impl;
-
-import com.xuyp.git.mode.factory.IFactory;
-
 /*
  * File Name: Factory.java
  * Created by xuyp on 2017年4月24日 下午5:52:33.
  * Copyright:Copyright © 1985-2017 xuyp Inc.All right reserved.
  */
+package com.xuyp.git.mode.factory.impl;
+
+import com.xuyp.git.mode.factory.IFactory;
+
 public class Factory510 implements IFactory {
 
 	@Override
 	public BMW createBMW() {
 		return new BMW510();
+	}
+	
+	public void abcd() {
+		
 	}
 }
